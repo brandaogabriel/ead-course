14b8362b32ecb6e46d34fa515334139b88a01a3b
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-03-26 12:07:15 -0300

    add fetch type to the relations

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,9 +1,28 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    /**
+     * With @EntityGraph annotation we can fetch associations like "course"
+     * of entity ModuleModel, even if the association is defined with FetchType LAZY.
+     * Works like an override to change the FetchType from LAZY to EAGER to the specific fetch below.
+     *
+     * @param title the module title
+     * @return the module entity with relation course
+     */
+    @EntityGraph(attributePaths = "course")
+    Optional<ModuleModel> findByTitle(String title);
+
+    @Query(value = "SELECT * FROM tb_modules WHERE course_course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
 }
