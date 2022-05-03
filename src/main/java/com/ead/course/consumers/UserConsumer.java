25baf90da7f852ed10854f8f3bcf8967955a7e2c
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-05-03 14:36:39 -0300

    create consumer to CREATE actionType when register a new user on ms-authuser

---
+++ src/main/java/com/ead/course/consumers/UserConsumer.java
@@ -0,0 +1,39 @@
+package com.ead.course.consumers;
+
+import com.ead.course.dtos.UserEventDto;
+import com.ead.course.enums.ActionType;
+import com.ead.course.services.UserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class UserConsumer {
+
+    private final UserService userService;
+
+    public UserConsumer(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
+            exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange.name}", type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true"))
+    )
+    public void listenUserEvent(@Payload UserEventDto event) {
+        log.info("EVENT - listUserEvent received, actionType {}", event.getActionType());
+        var userModel = event.toModel();
+
+        switch (ActionType.valueOf(event.getActionType())) {
+            case CREATE:
+                userService.save(userModel);
+                break;
+        }
+    }
+}
