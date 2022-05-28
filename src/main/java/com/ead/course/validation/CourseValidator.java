325a08ffb593e0fbac8c38d6c0135a0f82466289
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-05-28 20:21:41 -0300

    add validation when save course

--- src/main/java/com/ead/course/validation/CourseValidator.java
+++ src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,11 +1,13 @@
 package com.ead.course.validation;
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
+import com.ead.course.configs.security.UserDetailsImpl;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.enums.UserType;
 import com.ead.course.models.UserModel;
-import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.UserService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -18,10 +20,12 @@ public class CourseValidator implements Validator {
 
     private final Validator validator;
     private final UserService userService;
+    private final AuthenticationCurrentUserService authenticationCurrentUserService;
 
-    public CourseValidator(@Qualifier("defaultValidator") Validator validator, UserService userService) {
+    public CourseValidator(@Qualifier("defaultValidator") Validator validator, UserService userService, AuthenticationCurrentUserService authenticationCurrentUserService) {
         this.validator = validator;
         this.userService = userService;
+        this.authenticationCurrentUserService = authenticationCurrentUserService;
     }
 
     @Override
@@ -40,13 +44,18 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        Optional<UserModel> possibleUser = userService.findById(userInstructor);
-        if (possibleUser.isEmpty()) {
-            errors.rejectValue("userInstructor", "UserInstructorError", "Instructor nof found.");
-        }
-        var userModel = possibleUser.get();
-        if (userModel.getUserType().equals(UserType.STUDENT.toString())) {
-            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+        UserDetailsImpl currentUser = authenticationCurrentUserService.getCurrentUser();
+        if (currentUser.getUserId().equals(userInstructor)) {
+            Optional<UserModel> possibleUser = userService.findById(userInstructor);
+            if (possibleUser.isEmpty()) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found.");
+            }
+            var userModel = possibleUser.get();
+            if (userModel.getUserType().equals(UserType.STUDENT.toString())) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+            }
+        } else {
+            throw new AccessDeniedException("Forbidden");
         }
     }
 }
